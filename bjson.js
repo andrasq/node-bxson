56aec61e4304e7e77e3a2512c20112a73af7cdcc
Andras <andras@andrasq.com>
2023-01-22 02:50:56 -0500

    checkoint pushbuf and bjson

---
+++ bjson.js
@@ -0,0 +1,362 @@
+/*
+ * binary json encoding, for fast ipc
+ *
+ * Copyright (C) 2022,2023 Andras Radics
+ * Licensed under the Apache License, Version 2.0
+ *
+ * 2021-12-28 - AR.
+ * 2023-01-21 - support all length encodings, speed up, start decode
+ */
+
+/**
+----------------------------------------------------------------
+README.md
+----------------------------------------------------------------
+
+bxjson -- compact json-like binary serialization
+
+`bxjson` is a binary serialization format very in scope and intent to JSON.
+
+`bxjson` serializes arrays and objects using a variable-length encoding, the end of the data
+can only be determined by traversing the contained elements.
+
+----------------------------------------------------------------
+**/
+
+'use strict';
+
+var qibl = require('../qibl');
+var ieeeFloat = require('ieee-float');
+var PushBuffer = require('./pushbuf');
+
+/**
+// cf Msgpack, https://github.com/msgpack/msgpack/blob/master/spec.md
+// cf ../qbson/dev/qxpack, which is way too complicated and counts are not obvious
+// null, true, false, int, float, binary, array, object, <pad>
+
+simplifed:
+  // ---- immediate values -32..+31
+  intC                  00sxxxxx
+  // ---- up to 64 fixed-length types:
+* null                  01000000
+* false                 01000010
+* true                  01000011
+* int8                  01000100
+* int16                 01000101
+* int32                 01000110
+* float64               01001111
+  // ---- up to 32 length-counted types:
+* str8, str32           100000xx
+* blob8, blob32         100100xx
+* array8, array32       101000xx
+* object8, object32     101100xx
+  // ---- 4 immediate-length types:
+* strC                  1100xxxx
+* blobC                 1101xxxx
+* arrayC                1110xxxx
+* objectC               1111xxxx
+
+Transport format: a blob32 length-counted blob containing the encoded argument.
+The stored length is the size of the payload; the blob is length + 5 bytes total.
+
+**/
+
+var TYPE_MASK   = 0xC0;
+
+// immediate integers -32..31
+var TYPE_IMMEDIATE = 0x00;
+var MASK_IMMEDIATE = 0x3F;                      // 6 bits
+var MASK_IMMEDIATEYTPE = ~MASK_IMMEDIATE & 0xff;
+var IMMED_RANGE = (MASK_IMMEDIATE >> 1) + 1;    // -32 .. 31
+var T_INTC      = 0x00 + 0;     // 00Sxxxxx
+
+// fixed-length types (up to 64)
+// note that the integer types are arranged so that length-16 and -32 are 1 and 2 more than length-8 (mask 0x03)
+var TYPE_FIXLEN = 0x40;
+var T_NULL      = 0x40;         // 01000000
+var T_UNDEFINED = 0x41;
+var T_FALSE     = 0x42;
+var T_TRUE      = 0x43;
+var T_INT8      = 0x44;         // 010001xx
+var T_INT16     = 0x45;
+var T_INT32     = 0x46;
+var T_INT64     = 0x47;
+var T_FLOAT32   = 0x4E;
+var T_FLOAT64   = 0x4F;
+
+// length-counted types (up to 16)
+// note that the length-16 and -32 are always 1 and 2 more than length-8 (mask 0x03)
+var TYPE_BYTELEN = 0x80;
+var MASK_BYTELEN = 0x03;
+var MASK_BYTETYPE = 0xFC;
+var T_STR8      = 0x90;         // 100100xx
+var T_STR16     = 0x90 + 1;
+var T_STR32     = 0x90 + 2;
+var T_BLOB8     = 0x94;         // 100101xx
+var T_BLOB16    = 0x94 + 1;
+var T_BLOB32    = 0x94 + 2;
+var T_ARRAY8    = 0x98;         // 100110xx
+var T_ARRAY16   = 0x98 + 1;
+var T_ARRAY32   = 0x98 + 2;
+var T_OBJECT8   = 0x9C;         // 100111xx
+var T_OBJECT16  = 0x9C + 1;
+var T_OBJECT32  = 0x9C + 2;
+
+// immediate-length types (up to 4)
+// FIXME: fix the type/mask naming! type_lengthC vs mask_length_type vs mask_length_length
+var TYPE_IMMILEN = 0xC0;
+var MASK_IMMILEN = 0x0F;
+var MASK_IMMITYPE = 0xF0;
+var T_STRC      = 0xC0 + 0;     // 1100xxxx
+var T_BLOBC     = 0xD0 + 0;     // 1101xxxx
+var T_ARRAYC    = 0xE0 + 0;     // 1110xxxx
+var T_OBJECTC   = 0xF0 + 0;     // 1111xxxx
+
+
+function encode( item ) {
+    var buf = new PushBuffer();
+    encodeItem(buf, item);
+    return buf.slice();
+}
+
+function decode( bytes ) {
+    var buf = new PushBuffer(bytes);
+    return decodeItem(buf);
+}
+
+function encodeItem( buf, item ) {
+    switch (typeof item) {
+    case 'boolean':     buf.push(item ? T_TRUE : T_FALSE); break;
+    case 'number':      encodeNumber(buf, item); break;
+    case 'string':      encodeString(buf, item); break;
+    case 'object':
+        if (item === null) buf.push(T_NULL);
+        else switch (item.constructor) {
+        case Array:     encodeArray(buf, item); break;
+        case Date:      encodeItem(buf, item.toISOString()); break;
+        case Buffer:    encodeBlob(buf, item); break;
+        case Boolean: case Number: case String:
+                        encodeItem(buf, item.valueOf()); break;
+        default:        encodeObject(buf, item); break;
+        }
+        break;
+    default: // bigint (exception), symbol (undef), function (undef)
+        // hack: convert any unrecognized types to null (sort of like JSON in arrays;
+        // JSON converts unknowns to undefined, which are omitted from objects)
+        // buf.push(T_NULL); break;
+        if (item === undefined) buf.push(T_UNDEFINED); break;
+        buf.push(T_UNDEFINED); break;
+    }
+}
+
+function decodeItem( buf ) {
+    var type = buf.shift(1);
+    switch (type & TYPE_MASK) {
+    case TYPE_IMMEDIATE:
+        return (type & MASK_IMMEDIATE) << 26 >> 26;
+    case TYPE_FIXLEN:
+        switch (type) {
+        case T_NULL: return null;
+        case T_UNDEFINED: return undefined;
+        case T_FALSE: return false;
+        case T_TRUE: return true;
+        case T_INT8: case T_INT16: case T_INT32: case T_INT64:
+        case T_FLOAT32: case T_FLOAT64:
+            return decodeNumber(buf, type);
+        }
+    case TYPE_BYTELEN:
+        var len = buf.shift(1 << (type & MASK_BYTELEN)); // 0..3 meaning 1, 2, 4 or 8
+        switch (type & MASK_BYTETYPE) {
+        case T_STR8: return decodeString(buf, len);
+        case T_BLOB8: return decodeBlob(buf, len);
+        case T_ARRAY8: return decodeArray(buf, len);
+        case T_OBJECT8: return decodeObject(buf, len);
+        }
+    case TYPE_IMMILEN:
+        var len = type & MASK_IMMILEN; // 0..15 embedded into the type code
+        switch (type & MASK_IMMITYPE) {
+        case T_STRC: return decodeString(buf, len);
+        case T_BLOBC: return decodeBlob(buf, len);
+        case T_ARRAYC: return decodeArray(buf, len);
+        case T_OBJECTC: return decodeObject(buf, len);
+        }
+    }
+}
+
+// we arranged the type ids so that type16 and type32 can be computed from type8
+function encodeLength( buf, len, typeC, type8 ) {
+    if (len <= MASK_IMMILEN) buf.push(typeC + len);
+    else if (len < 256) buf.push(type8, len);
+    else if (len < 65536) buf.push(type8 + 1, len >> 8, len);
+    else buf.push(type8 + 2, len >> 24, len >> 16, len >> 8, len);
+}
+
+function decodeLength( buf, type ) {
+    if ((type & TYPE_MASK) === TYPE_IMMILEN) return type & MASK_IMMILEN;
+    else return buf.shift(1 << (type & MASK_BYTELEN));
+}
+
+function encodeNumber( buf, item ) {
+    if ((item | 0) !== item) {
+        buf.push(T_FLOAT64);
+        ieeeFloat.writeDoubleBE(buf.buf, item, buf.end);
+        buf.end += 8;
+    }
+    else if (item >= -IMMED_RANGE && item < IMMED_RANGE) buf.push(T_INTC + (item & MASK_IMMEDIATE));
+    else if (item >= -128 && item < 128) buf.push(T_INT8, item);
+    else if (item >= -32768 && item < 32768) buf.push(T_INT16, item >> 8, item);
+    else buf.push(T_INT32, item >> 24, item >> 16, item >> 8, item);
+}
+
+function decodeNumber( buf, type ) {
+    // immediates are handled in decodeItem
+    if (type === T_FLOAT64) return ieeeFloat.readDoubleBE(buf.buf, (buf.pos += 8) - 8);
+    else if (type === T_FLOAT32) return ieeeFloat.readFloatBE(buf.buf, (buf.pos += 4) - 4);
+    else switch (type & MASK_BYTELEN) {
+    case 0: return buf.shift(1) << 24 >> 24; break;
+    case 1: return buf.shift(2) << 16 >> 16; break;
+    case 2: return buf.shift(4) >> 0;
+    case 3: throw new Error('int64 not supported');
+    }
+}
+
+function encodeString( buf, item ) {
+    var len = PushBuffer.byteLength(item);
+    encodeLength(buf, len, T_STRC, T_STR8);
+    buf.pushString(item);
+}
+
+function decodeString( buf, len ) {
+    return buf.shiftString(len);
+}
+
+function encodeArray( buf, item ) {
+    var len = item.length;
+    encodeLength(buf, len, T_ARRAYC, T_ARRAY8);
+    for (var i = 0; i < len; i++) {
+        encodeItem(buf, item[i]);
+    }
+}
+
+function decodeArray( buf, len ) {
+    var arr = new Array(len);
+    for (var i = 0; i < len; i++) arr[i] = decodeItem(buf);
+    return arr;
+}
+
+function encodeObject( buf, item ) {
+    if (item.toJSON) { item = item.toJSON(); delete item.toJSON; return encodeItem(buf, item) }
+    if (item.constructor === Object) {
+        var len = 0;
+        // it is faster to walk the keys twice than to call Object.keys
+        // but not faster to encodeString() the key
+        for (var key in item) len += 1;
+        encodeLength(buf, len, T_OBJECTC, T_OBJECT8);
+        for (var key in item) {
+            encodeItem(buf, key), encodeItem(buf, item[key]);
+        }
+    }
+    else {
+        // Object.keys runs slow on older node
+        var keys = Object.keys(item), len = keys.length;
+        for (var i = 0; i < keys.length; i++) if (keys[i] === undefined) len -= 1;
+        encodeLength(buf, len, T_OBJECTC, T_OBJECT8);
+        for (var i = 0; i < keys.length; i++) {
+            var key = keys[i];
+            encodeItem(buf, key);
+            encodeItem(buf, item[key]);
+        }
+    }
+}
+
+function decodeObject( buf, len ) {
+    var obj = {};
+    for (var i = 0; i < len; i++) {
+        var key = decodeItem(buf);
+        obj[key] = decodeItem(buf);
+    }
+    return obj;
+}
+
+function encodeBlob( buf, item ) {
+    var mark = buf.end, len = item.length;
+    encodeLength(buf, len, T_BLOBC, T_BLOB8);
+    buf.pushBlob(item);
+//    buf._growBuf(len);
+//    for (var base = buf.end, i = 0; i < len; i++) buf[base + i] = item[i];
+}
+
+function decodeBlob( buf, len ) {
+    return buf.shiftBlob(len);
+}
+
+// /** quicktest:
+
+var fromBuf = parseFloat(process.versions.node) > '7' ? Buffer.from : Buffer;
+
+var assert = require('assert');
+var utf8 = require('utf8');
+var qutf8 = require('q-utf8');
+var qutf8b = require('../qbson/lib/utf8-2');
+var qibl = require('../qibl');
+var timeit = require('qtimeit');
+var qbson = require('../qbson');
+var msgpackjs = require('msgpackjs') // but fix to return buffers
+var msgpackjs_pack = msgpackjs.pack; msgpackjs.pack = function(v){ return fromBuf(msgpackjs_pack(v)) }
+
+// var x = encode({aaa: [1,2,3], b: "ABC"});
+// console.log("AR: got", x);
+
+var data = {a: 1.5, b: "foo", c: [1,2], d: true, e: {}};
+//var data = {a: 1.5, b: "foo", c: [1,2], d: true, e: {}};
+var data = {a: 1, bee: 2, ceeeeeeeeeeee: 3};
+var data = 123;
+var data = 1.5;
+var data = true;
+var data = [1,2];
+var data = {a:1};
+var data = "foobar";
+var data = {a:1, b:2, c:3, d:4, e:5};
+var data = {a: 1.5, b: "foo", c: [1,2e5,3e10], d: true, e: {f: {}}, g: "barbarbarbarbarbarbarbarbarbar"};
+var data = require('../ell/test/logline.json');
+var data = {foo: data, bar: data, bat: data, baz: data};
+
+//console.log("AR: data", data);
+var x = encode(data);
+console.log("AR: encoded as", x);
+//console.log("AR: decoded as", decode(x));
+for (var i=0; i<3; i++) {
+    var x1, x2, x3, x4;
+    var s = qibl.str_repeat("foo", 100);
+    var slen = s.length;
+    var target = [];
+if (0) {
+    timeit(100000, function() { x = utf8.encode(s) });
+    timeit(100000, function() { x = qutf8.encode(s, 0, slen, [], 0) });
+    timeit(100000, function() { x = qutf8b.write([], 0, s, 0, slen, false) });
+console.log("AR:", x);
+}
+
+    timeit(100000, function() { x1 = encode(data)         });
+    timeit(100000, function() { x2 = qbson.encode(data)   });
+    timeit(100000, function() { x3 = msgpackjs.pack(data) });
+    timeit(100000, function() { x4 = JSON.stringify(data) });
+console.log("AR: encoded", x1.length, x2.length, x3.length, x4.length);
+
+    timeit(100000, function() { x = decode(x1)           });
+    timeit(100000, function() { x = qbson.decode(x2)     });
+    timeit(100000, function() { x = msgpackjs.unpack(x3) });
+    timeit(100000, function() { x = JSON.parse(x4)       });
+console.log("AR: decoded");
+}
+
+assert.deepEqual(qibl.toArray(encode(123)), [T_INT8, 123]);
+assert.deepEqual(qibl.toArray(encode(-123)), [T_INT8, -123 & 0xff]);
+// assert.deepEqual(qibl.toArray(encode([1,2,3])), [T_ARRAYC + 3, T_INT8, 1, T_INT8, 2, T_INT8, 3]);
+assert.deepEqual(qibl.toArray(encode([1,-2,3])), [T_ARRAYC + 3, T_INTC + 1, T_INTC + 0x3e, T_INTC + 3]);
+
+//
+// 2023-01-21: 55% faster and 25% smaller than JSON.stringify (node-v13.8.0)
+//
+
+/**/
---
+++ pushbuf.js
@@ -0,0 +1,164 @@
+/*
+ * Buffer that can automatically grow like an Array, to capture variable-length output.
+ *
+ * Copyright (C) 2022,2023 Andras Radics
+ * Licensed under the Apache License, Version 2.0
+ */
+
+'use strict';
+
+var allocBuf = eval('parseFloat(process.versions.node) > 6 ? Buffer.allocUnsafe : Buffer');
+var fromBuf = eval('parseFloat(process.versions.node) > 6 ? Buffer.from : Buffer');
+
+module.exports = PushBuffer;
+
+// var xutf8 = require('utf8'); // generates wrong code for 'abc\u1234'
+var qutf8 = require('../q-utf8/utf8');
+
+/*
+ * Automatically growing buffers, like arrays.  Utf8 encoding and byteLength from q-utf8.
+ * Note that varargs is much much slower (6x) if a named arg is also declared.
+ */
+function PushBuffer( bytes ) {
+    this.capacity = 0;
+    this.buf = bytes || null;
+    this.end = bytes ? bytes.length : 0;
+    this.pos = 0;
+    this._allocBuf = allocBuf;
+}
+PushBuffer.prototype.push = function push(/* byte, byte, ... */) {
+    this._growBuf(arguments.length);
+    var si = 0, buf = this.buf;
+    switch (arguments.length) {
+    case 5: buf[this.end++] = arguments[si++] & 0xff;
+    case 4: buf[this.end++] = arguments[si++] & 0xff;
+    case 3: buf[this.end++] = arguments[si++] & 0xff;
+    case 2: buf[this.end++] = arguments[si++] & 0xff;
+    case 1: buf[this.end++] = arguments[si++] & 0xff;
+        break;
+    default:
+        for (var i = 0; i < arguments.length; i++) buf[this.end++] = arguments[i] & 0xff;
+        break;
+    }
+}
+// FIXME: use n = (n * 256) + buf[this.pos++] for integers > 32 bits
+// FIXME: tricky to recover a signed 64-bit integer because cannot use >> to sign-extend
+PushBuffer.prototype.shift = function shift( n ) {
+    var val = 0, buf = this.buf;
+    switch (n) {
+    default: throw new Error('cannot shift ' + n);
+    case 4: val = (val << 8) | buf[this.pos++];
+    case 3: val = (val << 8) | buf[this.pos++];
+    case 2: val = (val << 8) | buf[this.pos++];
+    case 1: val = (val << 8) | buf[this.pos++];
+    }
+    return val;
+}
+PushBuffer.prototype.pushString = function pushString(str) {
+    if (this.end + 3 * str.length > this.capacity) this._growBuf(PushBuffer.byteLength(str));
+    if (str.length <= 100 || !Buffer.isBuffer(this.buf)) {
+        // utf8 encoding extracted from q-utf8
+        var buf = this.buf, ix = this.end;
+        for (var len = str.length, i = 0; i < len; i++) {
+            var code = str.charCodeAt(i);
+            if (code <= 0x7F) {
+                buf[ix++] = code;
+            } else if (code <= 0x7FF) {
+                buf[ix++] = 0xC0 | (code >> 6); buf[ix++] = 0x80 | code & 0x3f;
+            } else if (code <= 0xD7FF || code >= 0xC000) {
+                buf[ix++] = 0xE0 | (code >> 12) & 0x0f; buf[ix++] = 0x80 | (code >> 6) & 0x3f; buf[ix++] = 0x80 | code & 0x3f;
+            } else if (code >= 0xD800 && code <= 0xDFFF) {
+                var code2 = string.charCodeAt(i + 1);
+                if (i + 1 < len && code <= 0xDBFF && code2 >= 0xDC00 && code2 <= 0xDFFF) {
+                    // valid leading,trailing surrogate pair containing a 20-bit code point
+                    var codepoint = 0x10000 + ((code - 0xD800) << 10) + (code2 - 0xDC00);
+                    buf[ix++] = 0xF0 | (codepoint >> 18) & 0x07;
+                    buf[ix++] = 0x80 | (codepoint >> 12) & 0x3F;
+                    buf[ix++] = 0x80 | (codepoint >>  6) & 0x3F;
+                    buf[ix++] = 0x80 | (codepoint      ) & 0x3F;
+                    i += 1;
+                } else {
+                    // lone leading surrogate or bare trailing surrogate are invalid, become FFFD
+                    buf[ix++] = 0xEF; buf[ix++] = 0xBF; buf[ix++] = 0xBD;
+                }
+            }
+        }
+        this.end = ix;
+    }
+    else {
+        this.end += this.buf.write(str, this.end, 'utf8');
+    }
+}
+PushBuffer.prototype.shiftString = function shiftString( len ) {
+    return qutf8.decodeUtf8(this.buf, this.pos, this.pos += len);
+}
+PushBuffer.prototype.pushBlob = function pushBlob( bytes ) {
+    var len = bytes.length;
+    this._growBuf(len);
+    for (var buf = this.buf, base = this.end, i = 0; i < len; i++) buf[base + i] = bytes[i];
+    this.end += i;
+console.log("AR: buf", buf);
+}
+PushBuffer.prototype.shiftBlob = function shiftBlob( len ) {
+    return this.buf.slice(this.pos, this.pos += len);
+}
+PushBuffer.prototype.slice = function slice(base, bound) {
+//    var buf = this._allocBuf(this.end);
+//    for (var i=0; i<this.end; i++) buf[i] = this.buf[i];
+//    return buf;
+    return this.buf.slice(base || 0, bound || this.end);
+}
+// string byte length
+// valid surrogate pair encodes 2 chars into 4 bytes (two codes, D800-DBFF followed by DC00-DFFF)
+// lone surrogate (not part of a valid pair) is encoded into 3 bytes as codepoint FFFD, handled by the (> 0x7FF) case
+// TODO: back-port this version into q-utf8 -- but NOTE: this inlined version is 6% faster than q-utf8
+// return qutf8.byteLength(s); 1.8m/s
+PushBuffer.byteLength = function byteLength(s) {
+    var len = s.length;
+    for (var code, code2, i = 0; i < s.length; i++) {
+        if ((code = s.charCodeAt(i)) > 0x7F) len += 1 + +(code > 0x7FF);
+        if (code >= 0xD800 && code < 0xDC00) {
+            if ((code2 = s.charCodeAt(i + 1)) >= 0xDC00 && code2 <= 0xDFFF) { len += 1; i++ }
+        }
+    }
+    return len;
+}
+PushBuffer.prototype._poke = function(/* ,varargs */) {
+    var end = arguments[0], buf = this.buf;
+    for (var i = 1; i < arguments.length; i++) buf[end++] = arguments[i] & 0xff;
+}
+PushBuffer.prototype._push = function(/* varargs */) {
+    for (var i = 0; i < arguments.length; i++) this.buf[this.end++] = arguments[i] & 0xff;
+}
+PushBuffer.prototype._growBuf = function(n) {
+    if ((this.end + n) > this.capacity) {
+        var oldbuf = this.buf;
+        this.capacity = (1.25 * this.capacity + 256 + 1.25 * n) >>> 0;
+        this.buf = this._allocBuf(this.capacity);
+        if (oldbuf) for (var i = 0; i < oldbuf.length; i++) this.buf[i] = oldbuf[i];
+    }
+}
+
+
+/** quicktest:
+
+var qtimeit = require('qtimeit');
+
+var x, buf = new PushBuffer();
+buf.pushString("foobar");
+console.log("AR: buf", buf);
+buf.push(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+qtimeit(.05, function() { buf.push(99) }); // 370m/s pushes
+var z = 0x12345678;
+qtimeit(.02, function() { buf.pushString("foobar blurblexy") }); // 24m/s
+//qtimeit(.05, function() { buf.push("foobar blurblexy"); x = buf.slice() }); // 28m/s
+//qtimeit(.05, function() { x = PushBuffer.byteLength("foobar \u1234lurblexy") }); // 51m/s
+qtimeit(.15, function() { buf.push(4, (z >> 24), (z >> 16), (z >> 8), z) }); // 90m/s pushes of 5 (w/o strings)
+//qtimeit(.15, function() { buf._poke(-1, 4, (z >> 24), (z >> 16), (z >> 8), z) }); // 185m/s
+qtimeit(.02, function() { buf.pushString("foobar \u1234lurblexy") }); // 24m/s (but sometimes 5m/s)
+//qtimeit(.12, function() { buf.pushString("\u1234\u1234\u1234\u1234") }); // 38m/s
+console.log("AR: buf", buf);
+//for (var i=10; i<60; i++) buf.push(i);
+//console.log("AR: buf", buf);
+
+/**/
